b8290968cb3554525879304087a21c2e694bc2c2
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-04-27 10:29:30 +0400

    Auto-connect using ServerSelector

--- modules/web/src/com/company/globaleventspoc/web/WebSocketClient.java
+++ modules/web/src/com/company/globaleventspoc/web/WebSocketClient.java
@@ -2,36 +2,89 @@ package com.company.globaleventspoc.web;
 
 import com.company.globaleventspoc.Message;
 import com.haulmont.cuba.core.sys.events.AppContextStoppedEvent;
+import com.haulmont.cuba.core.sys.remoting.discovery.ServerSelector;
+import com.haulmont.cuba.core.sys.remoting.discovery.StickySessionServerSelector;
 import com.haulmont.cuba.core.sys.serialization.SerializationSupport;
+import com.haulmont.cuba.web.security.events.AppStartedEvent;
 import org.atmosphere.wasync.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.util.Base64;
 
-@Component
+@Component("globevnt_WebSocketClient")
 public class WebSocketClient {
 
     private static final Logger log = LoggerFactory.getLogger(WebSocketClient.class);
 
     private Socket socket;
 
+    @Resource(name = ServerSelector.NAME)
+    private ServerSelector serverSelector;
+
     @Inject
     private UiNotifier uiNotifier;
+
+    @EventListener(AppStartedEvent.class)
+    public void init() {
+        // connect on first web request
+        connect();
+    }
+
+    @EventListener(AppContextStoppedEvent.class)
+    public void dispose() {
+        // disconnect on server shutdown
+        disconnect();
+    }
 
     public synchronized void connect() {
         if (socket != null)
             return;
         log.info("Opening socket");
+
+        Object context = serverSelector.initContext();
+        String url = getUrl(context);
+        if (url == null) {
+            throw new RuntimeException("Unable to open WebSocket: no available server URLs");
+        }
+        while (true) {
+            try {
+                tryConnect(url);
+                break;
+            } catch (IOException e) {
+                serverSelector.fail(context);
+                url = getUrl(context);
+                if (url != null)
+                    log.debug("Trying next URL");
+                else
+                    throw new RuntimeException("Unable to open WebSocket: no more server URLs available");
+            }
+
+        }
+    }
+
+    private String getUrl(Object context) {
+        String url = serverSelector.getUrl(context);
+        if (url != null && serverSelector instanceof StickySessionServerSelector) {
+            String servletPath = ((StickySessionServerSelector) serverSelector).getServletPath();
+            url = url.substring(0, url.lastIndexOf(servletPath));
+        }
+        return url;
+    }
+
+    private void tryConnect(String serverUrl) throws IOException {
+        log.debug("Connecting to " + serverUrl);
+
         Client client = ClientFactory.getDefault().newClient();
 
         RequestBuilder request = client.newRequestBuilder()
                 .method(Request.METHOD.GET)
-                .uri("http://localhost:8080/app-core/atmosphere")
+                .uri(serverUrl + "atmosphere")
                 .decoder(new Decoder<String, Message>() {
                     @Override
                     public Message decode(Event type, String str) {
@@ -63,32 +116,27 @@ public class WebSocketClient {
                 .transport(Request.TRANSPORT.LONG_POLLING);
 
         socket = client.create();
-        try {
-            socket
-                    .on("message", new Function<Message>() {
-                        @Override
-                        public void on(Message m) {
-                            log.info("message payload: " + m.getPayload());
-                            uiNotifier.sendMessage(m.getPayload());
-                        }
-                    })
-                    .on(new Function<Throwable>() {
-                        @Override
-                        public void on(Throwable t) {
-                            log.info("error: " + t);
-                        }
-                    })
-                    .on(new Function<String>() {
-                        @Override
-                        public void on(String o) {
-                            log.info("event: " + o);
-                        }
-                    })
-                    .open(request.build());
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        socket
+                .on("message", new Function<Message>() {
+                    @Override
+                    public void on(Message m) {
+                        log.info("message payload: " + m.getPayload());
+                        uiNotifier.sendMessage(m.getPayload());
+                    }
+                })
+                .on(new Function<Throwable>() {
+                    @Override
+                    public void on(Throwable t) {
+                        log.info("error: " + t);
+                    }
+                })
+                .on(new Function<String>() {
+                    @Override
+                    public void on(String o) {
+                        log.info("event: " + o);
+                    }
+                })
+                .open(request.build());
     }
 
     @EventListener(AppContextStoppedEvent.class)
